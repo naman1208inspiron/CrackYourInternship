1f63bb9a6f9c3a29d5519605ba036cc919fba7eb
Naman Sharma <74311341+naman1208inspiron@users.noreply.github.com>
2022-05-31 10:46:12 +0530

    Create 0013) Previous Smallest Element.cpp

---
+++ 0013) Previous Smallest Element.cpp
@@ -0,0 +1,13 @@
+vector<int> Solution::prevSmaller(vector<int> &A) {
+    stack<int> st;
+    vector<int> ans;
+    for(int i=0; i<A.size(); i++){
+        while(!st.empty() && st.top()>= A[i]) st.pop();
+        if(st.empty()){
+            ans.push_back(-1);
+        }
+        else ans.push_back(st.top());
+        st.push(A[i]);
+    }
+    return ans;
+}
