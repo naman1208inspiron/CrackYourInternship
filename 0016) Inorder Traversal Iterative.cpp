84a2597d2feb0afaf0169de73718859c1cbfecd0
Naman Sharma <74311341+naman1208inspiron@users.noreply.github.com>
2022-06-04 22:13:48 +0530

    Update 0016) Inorder Traversal Iterative.cpp

--- 0016) Inorder Traversal Iterative.cpp
+++ 0016) Inorder Traversal Iterative.cpp
@@ -18,3 +18,46 @@ public:
         return inorder;
     }
 };
+//````````````````````MORRIS TRAVERSAL``````````````````````````
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        // using morris traversal
+        vector<int> inorder;
+        TreeNode* curr= root;
+        while(curr){
+            if(curr->left== NULL){
+                inorder.push_back(curr->val);
+                curr= curr->right;
+            }
+            else{
+                TreeNode* prev= curr->left;
+                //making rightmost node of left subtree point to curr node before moving left
+                while(prev->right && prev->right!= curr){
+                    prev= prev->right;
+                }
+                if(prev->right== NULL){
+                    prev->right= curr;
+                    curr= curr->left;
+                }
+                else{
+                    prev->right= NULL;
+                    inorder.push_back(curr->val);
+                    curr= curr->right;
+                }
+            }
+        }
+        return inorder;
+    }
+};
