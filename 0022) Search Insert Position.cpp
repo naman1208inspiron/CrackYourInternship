9373ded26ff578ce83bb8ec918fcf1e44a4b096c
Naman Sharma <74311341+naman1208inspiron@users.noreply.github.com>
2022-06-11 21:18:21 +0530

    Update 0022) Search Insert Position.cpp

--- 0022) Search Insert Position.cpp
+++ 0022) Search Insert Position.cpp
@@ -16,3 +16,23 @@ public:
         return -1;
     }
 };
+//````````````````````````````````````````````````````````````````
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n= nums.size()-1;
+        
+        int lo=0, hi= n;
+        while(lo<=hi){
+            // if(target>nums[hi]) return hi+1;
+            // if(target<nums[lo]) return lo;
+            
+            int m= (lo+hi)/2;
+            if(nums[m]==target) return m;
+            else if(nums[m]>target) hi= m-1;
+            else lo= m+1;
+        }
+//         just return low
+        return lo;
+    }
+};
