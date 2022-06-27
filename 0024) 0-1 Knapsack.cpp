11da66246de4bfdac87ac7a8630a71b60ddf3a40
Naman Sharma <74311341+naman1208inspiron@users.noreply.github.com>
2022-06-27 18:02:26 +0530

    Create 0024) 0-1 Knapsack.cpp

---
+++ 0024) 0-1 Knapsack.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+    int dp[1001][1001];
+    int helper(int wt[], int val[], int w, int n){
+        if(n==0 || w==0) return 0;
+        
+        if(dp[w][n]!= -1) return dp[w][n];
+        if(wt[n-1] <= w){
+            int take= val[n-1] + helper(wt, val, w-wt[n-1], n-1);
+            int notTake= helper(wt, val, w, n-1);
+            return dp[w][n]= max(take, notTake);
+        }
+        return dp[w][n]= helper(wt, val, w, n-1);
+    }
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    {
+        memset(dp, -1, sizeof(dp));
+        // for me--> w= remaining weight, n= no of itmes left
+        return helper(wt, val, W, n);
+    }
+};
