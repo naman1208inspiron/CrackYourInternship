4198cd13ef71fc0c985754f71d60daab18b27ee9
Naman Sharma <74311341+naman1208inspiron@users.noreply.github.com>
2022-06-29 18:16:57 +0530

    Update 0005) Stickler Thief.cpp

--- random/0005) Stickler Thief.cpp
+++ random/0005) Stickler Thief.cpp
@@ -32,3 +32,24 @@ class Solution
         return max(findmax(arr, 0, true, n), findmax(arr, 0, false, n));
     }
 };
+//```````````````````small cute code```````````````
+class Solution
+{   // finds the max sum if the element is taken.
+    int dp[10001];
+    
+    int findmax(int arr[], int ind){
+        if(ind <= -1) return 0;
+        if(dp[ind]!= -1) return dp[ind];
+        
+        int option1= arr[ind] + findmax(arr, ind-2);
+        int option2= findmax(arr, ind-1);
+        return dp[ind] = max(option1, option2);
+    }
+    public:
+    int FindMaxSum(int arr[], int n)
+    {   
+        dp[n];
+        memset(dp, -1, sizeof(dp));
+        return findmax(arr, n-1);
+    }
+};
