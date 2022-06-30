a1555c905a155a7f90bf1f8a30cb4f387940e373
Naman Sharma <74311341+naman1208inspiron@users.noreply.github.com>
2022-06-30 22:45:53 +0530

    Create 0013) Min Sum formed by digits.cpp

---
+++ random/0013) Min Sum formed by digits.cpp
@@ -0,0 +1,13 @@
+class Solution{
+    public:
+    long long int minSum(int arr[], int n)
+    {
+        long long num1=0, num2=0;
+        sort(arr, arr+n);
+        for(int i=0; i<n; i++){
+            if(i%2==0) num1= num1*10 + arr[i];
+            else num2= num2*10 + arr[i];
+        }
+        return num1+num2;
+    }
+};
