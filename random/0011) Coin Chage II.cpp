a09b1584a4f3fbfc0642c47f6da2e3202af9a0c8
Naman Sharma <74311341+naman1208inspiron@users.noreply.github.com>
2022-06-30 17:13:01 +0530

    Create 0011) Coin Chage II.cpp

---
+++ random/0011) Coin Chage II.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    int dp[301][5001];
+    //finds the ways for getting amount using all the combos of coin at the current index.
+    int find(int ind, int amt, vector<int>& coins){
+        if(amt==0) return 1;
+        if(ind < 0) return 0;
+        
+        if(dp[ind][amt] != -1) return dp[ind][amt];
+        
+        int ways= 0;
+        for(int coinAmt= 0; coinAmt<= amt; coinAmt+= coins[ind]){
+            ways+= find(ind-1, amt-coinAmt, coins);
+        }
+        return dp[ind][amt] = ways;
+    }
+public:
+    int change(int amount, vector<int>& coins) {
+        memset(dp, -1, sizeof(dp));
+        int ans= find(coins.size()-1, amount, coins);
+        return ans;
+    }
+};
+
+
+
+//finds number of ways for getting desired amount.
+//     int find(int amt, vector<int> &coins){
+//         if(amt== 0) return 1;
+        
+//         int ways= 0;
+//         for(int i=0; i<coins.size(); i++){
+//             if(amt >= coins[i]){
+//                 ways+= find(amt- coins[i], coins);
+//             }
+//         }
+//         return ways;
+//     }
+//----> the above code will also consider the duplicates combination. 
