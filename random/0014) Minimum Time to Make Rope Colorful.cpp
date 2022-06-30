596866cf4a457185fd0fbf80d715dffe358e5865
Naman Sharma <74311341+naman1208inspiron@users.noreply.github.com>
2022-06-30 23:41:02 +0530

    Create 0014) Minimum Time to Make Rope Colorful.cpp

---
+++ random/0014) Minimum Time to Make Rope Colorful.cpp
@@ -0,0 +1,24 @@
+// idea--> when duplicates(same consecutive colors) are found, start adding their time and keep track of their max time. subtract max time from the total sum of time of duplicates and add to the answer. we will only keep the duplicate with maximum time. 
+class Solution {
+public:
+    int minCost(string colors, vector<int>& time) {
+        int n= time.size();
+        
+        int lo= 0, hi= 0, mini=0;
+        while(hi< n){
+            int currMax= 0;
+            int tsum=0;
+            // in case of unique elements --> tsum and currMax will be same--> so adding 0. 
+            while(colors[lo]== colors[hi]){
+                tsum+= time[hi];
+                currMax= max(currMax, time[hi]);
+                hi++;
+            }
+            
+            mini+= tsum-currMax;
+            // putting lo to the unique character, where hi is already present. 
+            lo= hi; 
+        }
+        return mini;
+    }
+};
