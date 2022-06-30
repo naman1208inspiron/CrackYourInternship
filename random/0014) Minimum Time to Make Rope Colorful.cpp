e97c2e81916630119e274362200e8244da8ad758
Naman Sharma <74311341+naman1208inspiron@users.noreply.github.com>
2022-07-01 00:05:36 +0530

    Update 0014) Minimum Time to Make Rope Colorful.cpp

--- random/0014) Minimum Time to Make Rope Colorful.cpp
+++ random/0014) Minimum Time to Make Rope Colorful.cpp
@@ -4,12 +4,13 @@ public:
     int minCost(string colors, vector<int>& time) {
         int n= time.size();
         
-        int lo= 0, hi= 0, mini=0;
-        while(hi< n){
+        int lo= 0, hi, mini=0;
+        while(lo< n){
             int currMax= 0;
             int tsum=0;
+            hi = lo;
             // in case of unique elements --> tsum and currMax will be same--> so adding 0. 
-            while(colors[lo]== colors[hi]){
+            while(hi<n && colors[lo]== colors[hi]){
                 tsum+= time[hi];
                 currMax= max(currMax, time[hi]);
                 hi++;
