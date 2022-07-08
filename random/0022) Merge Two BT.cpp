9fa7a1bf8fd6279e62161ca675f71889a00c73b7
Naman Sharma <74311341+naman1208inspiron@users.noreply.github.com>
2022-07-08 14:12:42 +0530

    Create 0022) Merge Two BT.cpp

---
+++ random/0022) Merge Two BT.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        return merge(root1, root2);
+    }
+    TreeNode* merge(TreeNode* root1, TreeNode* root2){
+        
+        if(!root1 && !root2) return NULL;
+        if(!root1) return root2;
+        if(!root2) return root1;
+        
+        int sum= 0;
+        if(root1) sum+= root1->val;
+        if(root2) sum+= root2->val;
+            
+        TreeNode* newRoot= new TreeNode(sum);
+        newRoot->left= merge(root1->left, root2->left);
+        newRoot->right= merge(root1->right, root2->right);
+        
+        return newRoot;
+    }
+};
