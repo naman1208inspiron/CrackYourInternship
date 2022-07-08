acc7ff2eb8a2e4aa44c32801e36871eab3fd6317
Naman Sharma <74311341+naman1208inspiron@users.noreply.github.com>
2022-07-08 14:20:54 +0530

    Update 0022) Merge Two BT.cpp

--- random/0022) Merge Two BT.cpp
+++ random/0022) Merge Two BT.cpp
@@ -20,3 +20,25 @@ public:
         return newRoot;
     }
 };
+
+
+//`````````````````````````````````````````````````````````
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        return merge(root1, root2);
+    }
+    TreeNode* merge(TreeNode* root1, TreeNode* root2){
+        
+        // if(!root1 && !root2) return NULL;
+        if(!root1) return root2;
+        if(!root2) return root1;
+        
+        root1->val+= root2->val;
+            
+        root1->left= merge(root1->left, root2->left);
+        root1->right= merge(root1->right, root2->right);
+        
+        return root1;
+    }
+};
